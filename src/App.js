d135f8fe0f8e784614813b2fbcb4cc95703e8515
jonathan41 <649368763@qq.com>
2024-04-01 23:20:50 +0800

    tijiao

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './home'
-import { useState } from 'react';
+import React from "react";
 
-function App() {
-  const [name, setName] = useState({name:'龙军林4444777'});
-  console.log(name,setName);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  render(){
+    return(
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload111.
+          hello React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <Home {...name} />
-      </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
